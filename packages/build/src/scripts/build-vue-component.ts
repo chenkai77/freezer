2f2c9a65a4b658ec5534def5e9b0e232d1858f2b
陈凯 <depp.chen@goldentec.com>
2022-07-20 10:14:33 +0800

    update

--- packages/build/src/scripts/build-vue-component.ts
+++ packages/build/src/scripts/build-vue-component.ts
@@ -4,15 +4,17 @@ import { rollup } from "rollup";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import typescript from 'rollup-plugin-typescript2';
-import vuePlugin from 'rollup-plugin-vue'
 
 async function buildVueComponent() {
+  
+
+
   const inputPath = path.resolve(process.cwd(), 'components', 'index.ts')
   try {
     const bundle = await rollup({
       input: inputPath,
-      plugins: [typescript(), vuePlugin()],
-      external: ['vue']
+      plugins: [typescript(), vue({isProduction: true,}), vueJsx()],
+      external: ['vue'],
     })
   
     await bundle.write({
@@ -22,6 +24,9 @@ async function buildVueComponent() {
       entryFileNames: `[name].js`,
       preserveModules: true,
       preserveModulesRoot: path.resolve(process.cwd(), 'components'),
+      globals: {
+        vue: "Vue" 
+      }
     })
   } catch (error) {
     console.log(error) 
